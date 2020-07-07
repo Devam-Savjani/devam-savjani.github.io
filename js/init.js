7a68d8eea49df167d4ab397d262df915f2e61ccf
Devam Savjani <devamsavjani@rocketmail.com>
2020-07-07 20:32:15 +0100

    Initial commit

---
+++ js/init.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    M.Parallax.init(elems, {responsiveThreshold:-100});
+    })
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(elems, {})});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(elems, {closeOnClick: true})});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.scrollspy');
+    M.ScrollSpy.init(elems, {})});    
